f5f7703207f1ea493f2e3f104297904fcfc7ed44
Novout <novout@hotmail.com>
2021-03-02 10:19:05 -0300

    feat(keyboard): pixi.js-keyboard implement

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import Mouse from './mouse';
+import Keyboard from './keyboard';
 
 class PIXIController {
   public Mouse: Mouse;
+  public Keyboard: Keyboard;
 
   constructor() {
     this.Mouse = new Mouse();
+    this.Keyboard = new Keyboard();
   }
 
   public update() {
     this.Mouse.update();
+    this.Keyboard.update();
+  }
+
+  public reset() {
+    this.Mouse.reset();
+    this.Keyboard.reset();
   }
 }
 
--- src/keyboard.ts
+++ src/keyboard.ts
@@ -1,1 +1,94 @@
-export default class Keyboard {}
+import Events from 'nom-events';
+export default class Keyboard {
+  private keyStates: Map<any, any>;
+  private events: any;
+  private keyDownListener = (event: any) => {
+    if (!this.keyStates.get(event.code)) {
+      this.keyStates.set(event.code, event);
+      this.events.call('pressed', event.code, event);
+      this.events.call('pressed_' + event.code, event.code, event);
+    }
+  };
+  private keyUpListener = (event: any) => {
+    event = this.keyStates.get(event.code);
+    if (event) {
+      event.wasReleased = true;
+      this.events.call('released', event.code, event);
+      this.events.call('released_' + event.code, event.code, event);
+    }
+  };
+
+  constructor() {
+    this.keyStates = new Map();
+    this.events = new Events();
+    this.set();
+  }
+
+  public clear() {
+    this.keyStates.clear();
+  }
+
+  public update() {
+    this.keyStates.forEach((value, keyCode) => {
+      const event = this.keyStates.get(keyCode);
+
+      event.alreadyPressed = true;
+
+      if (event.wasReleased) {
+        this.keyStates.delete(keyCode);
+      }
+
+      this.events.call('down', keyCode, event);
+      this.events.call('down_' + keyCode, keyCode, event);
+    });
+  }
+
+  public isKeyDown(...args: Array<any>) {
+    let result = false;
+    for (const keyCode of args) {
+      const event = this.keyStates.get(keyCode);
+      if (event && !event.wasReleased) result = true;
+    }
+
+    return result;
+  }
+
+  public isKeyUp(...args: Array<any>) {
+    return !this.isKeyDown(args);
+  }
+
+  public isKeyPressed(...args: Array<any>) {
+    let result = false;
+
+    if (args.length == 0) return false;
+
+    for (const keyCode of args) {
+      const event = this.keyStates.get(keyCode);
+      if (event && !event.wasReleased && !event.alreadyPressed) result = true;
+    }
+
+    return result;
+  }
+
+  public isKeyReleased(...args: Array<any>) {
+    let result = false;
+
+    if (args.length == 0) return false;
+
+    for (const keyCode of args) {
+      const event = this.keyStates.get(keyCode);
+      if (event && event.wasReleased) result = true;
+    }
+
+    return result;
+  }
+
+  private set() {
+    window.addEventListener('keydown', this.keyDownListener, false);
+    window.addEventListener('keyup', this.keyUpListener, false);
+  }
+
+  public reset() {
+    this.clear();
+  }
+}
