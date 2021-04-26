d02eae6dbdfe2846a45ea54d6441893fd4e398ac
Novout <novout@hotmail.com>
2021-04-26 16:15:55 -0300

    chore: rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,51 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
+import babel from 'rollup-plugin-babel';
+import dts from 'rollup-plugin-dts';
+import pkg from './package.json';
 
-const optionsForPackage = [''];
+const optionsForCompile = ['esm', 'cjs', 'iife'];
+
 const createPlugins = () => {
-  return [typescript(), resolve(), commonjs({ include: /node_modules/ })];
+  return [typescript(), json(), resolve(), commonjs(), babel({ exclude: 'node_modules/**' })];
 };
+
+const banner = [
+  `/*!`,
+  ` * ${pkg.name} - v${pkg.version}`,
+  ` *`,
+  ` * ${pkg.name} is licensed under the MIT License.`,
+  ` * http://www.opensource.org/licenses/mit-license`,
+  ` */`,
+].join('\n');
 
 const createConfig = () => {
   const list = [];
 
-  optionsForPackage.forEach((option) => {
+  optionsForCompile.forEach((format) => {
     list.push({
-      input: `./src${option}/index.ts`,
+      input: `./src/index.ts`,
       output: {
-        file: `./lib${option}/index.js`,
-        format: 'esm',
+        file: `./lib/index.${format}.js`,
+        format,
         exports: 'named',
+        banner,
+        sourcemap: true,
+        name: '_pixi_factory',
       },
-      plugins: createPlugins(),
+      plugins: [...createPlugins()],
     });
+  });
 
-    list.push({
-      input: `./src${option}/index.ts`,
-      output: {
-        file: `./lib${option}/index.min.js`,
-        format: 'esm',
-        exports: 'named',
-      },
-      plugins: [...createPlugins(), terser()],
-    });
-
-    list.push({
-      input: `./src${option}/index.ts`,
-      output: {
-        file: `./lib${option}/index.amd.js`,
-        format: 'amd',
-        exports: 'named',
-      },
-      plugins: createPlugins(),
-    });
-
-    list.push({
-      input: `./src${option}/index.ts`,
-      output: {
-        file: `./lib${option}/index.amd.min.js`,
-        format: 'amd',
-        exports: 'named',
-      },
-      plugins: [...createPlugins(), terser()],
-    });
+  list.push({
+    input: 'src/index.ts',
+    output: {
+      file: 'lib/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
   });
 
   return list;
